3f66458ed3f3568ad01d28736ccb0a0ba40732b4
SlightSweet <yatuoli0828@gmail.com>
2025-12-01 09:06:56 +0800

    更改

---
+++ src/main/java/edu_ngd_pdd/communitypddplatform/CommunityPddPlatformApplication.java
@@ -0,0 +1,13 @@
+package edu_ngd_pdd.communitypddplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityPddPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityPddPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu_ngd_pdd/communitypddplatform/CommunityPddPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package edu_ngd_pdd.communitypddplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityPddPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
