cb3ca66b902a902289c45e14bcca1faea42329c0
SlightSweet <yatuoli0828@gmail.com>
2025-12-02 11:10:41 +0800

    更改

---
+++ leader-service/src/main/java/leader/LeaderServiceApplication.java
@@ -0,0 +1,11 @@
+package leader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeaderServiceApplication.class, args);
+    }
+}
