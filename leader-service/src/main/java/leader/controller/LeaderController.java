72fa5d502f494864370e25ede644784842bc3a5a
SlightSweet <yatuoli0828@gmail.com>
2025-12-02 10:19:50 +0800

    更改

--- leader-service/src/main/java/leader/controller/LeaderController.java
+++ leader-service/src/main/java/leader/controller/LeaderController.java
@@ -1,4 +1,29 @@
 package leader.controller;
 
+import leader.service.LeaderService;
+import leader.pojo.Leader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/leader")
 public class LeaderController {
+    private final LeaderService leaderService;
+
+    // 构造器注入（推荐）
+    public LeaderController(LeaderService leaderService) {
+        this.leaderService = leaderService;
+    }
+
+    @RequestMapping("/findAllLeaders")
+    public List<Leader> findAllLeaders() {
+        return leaderService.findAllLeaders();
+    }
+
+    @RequestMapping("/findLeaderById")
+    public Leader findLeaderById(Integer id) {
+        return leaderService.findLeaderById(id);
+    }
 }
