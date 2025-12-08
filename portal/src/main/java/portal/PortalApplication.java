e826bd7de9b36e3eb498a02aa1fa473ab39c315f
SlightSweet <yatuoli0828@gmail.com>
2025-12-08 08:39:23 +0800

    门户启动项

--- portal/src/main/java/portal/PortalApplication.java
+++ portal/src/main/java/portal/PortalApplication.java
@@ -10,5 +10,6 @@ public class PortalApplication {
     public static void main(String[] args) {
         SpringApplication.run(PortalApplication.class, args);
     }
+//    启动此类并打开http://localhost:1010/portal.html
 }
 
